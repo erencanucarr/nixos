8e79d4e24d9bf545b7a70ad40764eb689fc3e569
Eren Can <eren@canucar.dev>
2026-07-08 22:01:16 +0300

    feat: switch stylix theme from Catppuccin to chalk

--- modules/desktop/stylix.nix
+++ modules/desktop/stylix.nix
@@ -2,7 +2,7 @@
 {
   stylix = {
     enable = true;
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/chalk.yaml";
     cursor.package = pkgs.bibata-cursors;
     cursor.name = "Bibata-Modern-Classic";
     cursor.size = 24;
